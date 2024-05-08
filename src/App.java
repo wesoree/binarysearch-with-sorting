915d87b9cbf06b95abc7cfd91df8a2dd8dc20eb7
wesoree <wesleylo174@gmail.com>
2024-05-07 21:14:59 -0400

    update App.java

--- src/App.java
+++ src/App.java
@@ -1,17 +1,41 @@
 import java.util.Arrays;
 public class App {
     
-    public static void bubbleSort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i]
-                    nums[i] = nums[j]
-                    nums[j] = temp;
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
                 }
             }
         }
-        System.out.println(nums);
+        return arr;
+    }
+
+    static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void sort(int arr[], int start, int end) {
+        if (start < end) { 
+            int pIndex = partition(arr, start, end);
+            sort(arr, start, pIndex - 1);
+            sort(arr, pIndex + 1, end);
+        }
+    }   
+
+    static int partition(int arr[], int start, int end) {
+        int pivot = arr[end];
+        int pIndex = start;
+        for (int i = start; i < end; i++) {
+            if (arr[i] <= pivot) {
+                swap(arr, i, pivot);
+                pIndex++;
+            }
+        }
+        return pIndex;
     }
 
     public static int binarySearch(int[] nums, int key) {
@@ -29,8 +53,20 @@ public class App {
         }
         return -1;
     }
+
+    public static void searchSort(int[] arr, int key){
+        int start = 0;
+        int end = arr.length - 1;
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("the number is located on spot " + binarySearch(arr, key));
+    }
+
     public static void main(String[] args) throws Exception {
-        int[] a = {5, 3, 7, 9, 3, 5};
-        bubbleSort(a);
+        int[] a = {5, 3, 7, 9, 3, 5, 2, 5, 3, 6, 12, 6, 3, 45, 100};
+        int[] b = bubbleSort(a);
+        String str = Arrays.toString(b);
+        System.out.println(str);
+        searchSort(a, 5);
     } 
 }
